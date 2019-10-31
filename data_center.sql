0930b55e9794e71578b8d3dad71518968e9d2fed
Renato Matsuda Vassão Dias <renato.dias@cubos.io>
2019-10-31 13:08:59 -0300

    add TODOs and remove unneeded properties

--- data_center.sql
+++ data_center.sql
@@ -1,3 +1,10 @@
+-- TODO: INSERT CREATE DATABASE STATEMENT
+-- TODO: ADD STATUS PROPERTY
+-- TODO: MAKE MORE INSERT STATEMENTS
+-- TODO: CREATE SELECT STATEMENTS
+-- TODO: DRAW CONCEPTUAL MODEL
+-- TODO: FOLLOW CONCEPTUAL MODEL
+
 CREATE TABLE racks (
     rack_id  int primary key
 );
@@ -10,15 +17,14 @@ CREATE TABLE load_balancers (
 CREATE TABLE servers (
     blade_id            int primary key,
     rack_id             int references racks(rack_id),
-    virtual_machine_id  int
 );
 
 CREATE TABLE virtual_machines (
     virtual_machine_id  int primary key,
-    app_instance_id     int,
     blade_id            int references servers(blade_id)
 );
 
+-- TODO: MAKE N-M RELATION WITH VM
 CREATE TABLE apps (
     app_id              int primary key,
     virtual_machine_id  int references virtual_machines(virtual_machine_id)
@@ -48,15 +54,15 @@ INSERT INTO racks VALUES (2);
 INSERT INTO load_balancers VALUES (1, 1);
 INSERT INTO load_balancers VALUES (2, 2);
 
-INSERT INTO servers VALUES (1, 1, 0);
-INSERT INTO servers VALUES (2, 1, 0);
-INSERT INTO servers VALUES (3, 2, 0);
+INSERT INTO servers VALUES (1, 1);
+INSERT INTO servers VALUES (2, 1);
+INSERT INTO servers VALUES (3, 2);
 
-INSERT INTO virtual_machines VALUES (10, 0, 1);
-INSERT INTO virtual_machines VALUES (11, 0, 2);
-INSERT INTO virtual_machines VALUES (20, 0, 2);
-INSERT INTO virtual_machines VALUES (30, 0, 2);
-INSERT INTO virtual_machines VALUES (31, 0, 3);
+INSERT INTO virtual_machines VALUES (10, 1);
+INSERT INTO virtual_machines VALUES (11, 2);
+INSERT INTO virtual_machines VALUES (20, 2);
+INSERT INTO virtual_machines VALUES (30, 2);
+INSERT INTO virtual_machines VALUES (31, 3);
 
 INSERT INTO apps VALUES (1, 10);
 -- INSERT INTO apps VALUES (1, 11);
