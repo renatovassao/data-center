d9a8dcdba975f67dbae9119572873ad6c2f216aa
Renato Matsuda Vassão Dias <renato.dias@cubos.io>
2019-10-31 00:35:13 -0300

    add pks and fks

--- data_center.sql
+++ data_center.sql
@@ -1,43 +1,43 @@
-CREATE TABLE databases (
-    database_id  int
-);
-
-CREATE TABLE app_databases (
-    app_id       int,
-    database_id  int
-);
-
-CREATE TABLE apps (
-    app_id              int,
-    virtual_machine_id  int
-);
-
-CREATE TABLE user_apps (
-    user_id  int,
-    app_id   int
-);
-
-CREATE TABLE users (
-    user_id  int
-);
-
-CREATE TABLE virtual_machine (
-    virtual_machine_id  int,
-    app_instance_id     int,
-    blade_id            int
-);
-
-CREATE TABLE servers (
-    blade_id            int,
-    rack_id             int,
-    virtual_machine_id  int
-);
-
 CREATE TABLE racks (
-    rack_id  int
+    rack_id  int primary key
 );
 
 CREATE TABLE load_balancers (
-    load_balancer_id    int,
-    rack_id             int
+    load_balancer_id    int primary key,
+    rack_id             int references racks(rack_id)
+);
+
+CREATE TABLE servers (
+    blade_id            int primary key,
+    rack_id             int references racks(rack_id),
+    virtual_machine_id  int
+);
+
+CREATE TABLE virtual_machines (
+    virtual_machine_id  int primary key,
+    app_instance_id     int,
+    blade_id            int references servers(blade_id)
+);
+
+CREATE TABLE apps (
+    app_id              int primary key,
+    virtual_machine_id  int references virtual_machines(virtual_machine_id)
+);
+
+CREATE TABLE databases (
+    database_id  int primary key
+);
+
+CREATE TABLE app_databases (
+    app_id       int references apps(app_id),
+    database_id  int references databases(database_id)
+);
+
+CREATE TABLE users (
+    user_id  int primary key
+);
+
+CREATE TABLE user_apps (
+    user_id  int references users(user_id),
+    app_id   int references apps(app_id)
 );
